68d5d0cb9d8e57ba0222668fe490c8104ed73f55
lavgin7 <oponeuser7@gmail.com>
2021-08-01 16:07:57 +0900

    feat: Route root page.

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from bson.json_util import dumps
 import pymongo
 
+def page_not_found(e):
+    return render_template('index.html'), 404
+
 app = Flask(__name__)
+
+app.register_error_handler(404, page_not_found)
 
 conn = pymongo.MongoClient("localhost", 27017)
 db = conn.thoth_db
