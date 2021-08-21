79482c5f6bd0bd194fa1e8693d4b80c0b67e2480
lavgin7 <oponeuser7@gmail.com>
2021-08-21 14:38:45 +0900

    feat: POST(api/signin)

--- blueprint.py
+++ blueprint.py
@@ -1,20 +1,21 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, make_response
 from bson.json_util import dumps
-import pymongo
+import pymongo, jwt
 
 bp = Blueprint('main', __name__, url_prefix='/api')
 
 conn = pymongo.MongoClient("localhost", 27017)
 db = conn.thoth_db
-col = db.postit
 
 @bp.route('/notes', methods=['GET'])
 def get():
+    col = db.postit
     data = list(col.find())
     return dumps(data, ensure_ascii=False)
 
 @bp.route('/notes', methods=['POST'])
 def post():
+    col = db.postit
     postit = {}
     postit['title'] = request.form['title']
     postit['code'] = request.form['code']
@@ -22,3 +23,19 @@ def post():
     col.insert_one(postit)
     return '{"error":null}'
 
+@bp.route('/signin', methods=['POST'])
+def login():
+    col = db.user
+    user = {}
+    user['email'] = request.form['email']
+    user['password'] = request.form['password']
+    result = col.find_one(user)
+    if result:
+        data = {}
+        data['email'] = user['email']
+        encoded = jwt.encode(data, 'JEfWefI0E1qlnIz06qmob7cZp5IzH/i7KwOI2xqWfhE=', algorithm='HS256')
+        resp = make_response("Login successful.")
+        resp.set_cookie('accessToken', encoded)
+        return resp
+    else:
+        return 'User not found.'
