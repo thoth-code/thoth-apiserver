439ae1be4ad87e225c6613554ac7061fb3f2f504
lavgin7 <oponeuser7@gmail.com>
2021-08-21 14:40:09 +0900

    feat: Specifying template

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 def page_not_found(e):
     return render_template('index.html'), 404
-    
+
 def create_app():
     app = Flask(__name__)
     app.register_error_handler(404, page_not_found)
